4e78f5bc66bb0a566b972046c1a5797f1249f9ae
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-11-22 19:59:34 +0100

    Add info about process names to the logger

--- lego_sorter_server/__main__.py
+++ lego_sorter_server/__main__.py
@@ -13,11 +13,22 @@ def exception_handler(exc_type, value, tb):
     logging.exception(f"Uncaught exception: {str(value)}")
 
 
+def configureLogging(logger):
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(fmt='[%(levelname)s][%(processName)s]%(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--brick_category_config", "-c", help='.json file with brick-category mapping specification', type=str, required=False)
     args = parser.parse_args()
-    logging.getLogger().setLevel(logging.INFO)
+
+    configureLogging(logging.getLogger())
+
     sys.excepthook = exception_handler
     threading.excepthook = exception_handler
+
     asyncio.run(Server.run(BrickCategoryConfig(args.brick_category_config)))
